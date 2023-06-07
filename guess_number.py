a61df2d1f2d945977b7e49426d3da5a81623e9b7
Carlos Galindo <utm22030587@utma.edu.mx>
2023-06-06 21:36:23 -0600

    Docs: Se añaden los comentarios.

--- guess_number.py
+++ guess_number.py
@@ -1,39 +1,39 @@
 print("\n***FIND THE NUMBER***\n")
 print("Instructions. You have to select a range, a number is going to be selected and you have to find the number before running out of points or you will lose.\n")
 
-from random import randint
+from random import randint			# Libreria usada para generar el número random
 
-again="y"
+again="y"							# Variable que usará el ciclo para repetir el juego
 
-while again=="y":
+while again=="y":					# Ciclo para repetir el juego
 
-	minNumber = int(input("Write the miminum value of the range: "))
-	maxNumber = int(input("Write the maximum value of the range: "))
-	randomNumber = randint(minNumber, maxNumber)
+	minNumber = int(input("Write the miminum value of the range: "))			# Minimo rango
+	maxNumber = int(input("Write the maximum value of the range: "))			# Maximo Rango
+	randomNumber = randint(minNumber, maxNumber)								# Numero generado
 	print("\nThe number has been selected.\n")
 
-	puntos=100
-	i=1
-	num=randomNumber+1
+	puntos=100								# Variable para los puntos
+	i=1										# Contador de oportunidades
+	num=randomNumber+1						# Variable con valor distinto al numero aleatorio para entrar al juego
 
-	while num!=randomNumber:
-		if puntos > 0:
-			num = int(input("Attempt {}, enter a number: ".format(i)))
-			if num > randomNumber:
+	while num!=randomNumber:				# Ciclo donde se adivina el numero
+		if puntos > 0:						# Si los puntos son mayores a 0 puede jugar, de lo contario habrá perdido
+			num = int(input("Attempt {}, enter a number: ".format(i)))		# Intento 'i', ingresa un numero
+			if num > randomNumber:											# Si el numero ingresado es mayor, dará una pista y restará 10 puntos
 				print("\nThe number is less\n")
 				puntos=puntos-10
 				print("Points: ",puntos)
 				i=i+1
-			elif num<randomNumber:
+			elif num<randomNumber:											# Si el numero ingresado es menor, dará una pista y restará 10 puntos
 				print("\nThe number is greater\n")
 				puntos=puntos-10
 				print("Points: ",puntos,"\n")
 				i=i+1
-			else:
+			else:															# De lo contrario habrá ganado
 				print("\nYOU HAVE WON IN",i,"ATTEMPTS.\n")
 				print("Final points: ",puntos,"\n")	
 		else:
-			print("\nYou lost, the number was: ",randomNumber,"\n")
-			num=randomNumber
+			print("\nYou lost, the number was: ",randomNumber,"\n")			# Al perder muestra el número random
+			num=randomNumber												# Num toma el mismo valor que el random para salir del juego
 
-	again=input("Do you want to play again?(y/n): ")
+	again=input("Do you want to play again?(y/n): ")						# Pregunta si quiere jugar de nuevo
