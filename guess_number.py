d20c1774d3b453073b3769d0ac35e4301c0b4108
Carlos Galindo <utm22030587@utma.edu.mx>
2023-06-06 21:29:51 -0600

    Feat: Se añade ciclo para repetir el juego mientras el usuario desee.

--- guess_number.py
+++ guess_number.py
@@ -3,33 +3,37 @@ print("Instructions. You have to select a range, a number is going to be selecte
 
 from random import randint
 
+again="y"
 
+while again=="y":
 
-minNumber = int(input("Write the miminum value of the range: "))
-maxNumber = int(input("Write the maximum value of the range: "))
-randomNumber = randint(minNumber, maxNumber)
-print("\nThe number has been selected.\n")
+	minNumber = int(input("Write the miminum value of the range: "))
+	maxNumber = int(input("Write the maximum value of the range: "))
+	randomNumber = randint(minNumber, maxNumber)
+	print("\nThe number has been selected.\n")
 
-puntos=100
-i=1
-num=randomNumber+1
-while num!=randomNumber:		
-	if puntos > 0:
-		num = int(input("Attempt {}, enter a number: ".format(i)))
-		if num > randomNumber:
-			print("\nThe number is less\n")
-			puntos=puntos-10
-			print("Points: ",puntos)
-			i=i+1
-		elif num<randomNumber:
-			print("\nThe number is greater\n")
-			puntos=puntos-10
-			print("Points: ",puntos,"\n")
-			i=i+1
+	puntos=100
+	i=1
+	num=randomNumber+1
+
+	while num!=randomNumber:
+		if puntos > 0:
+			num = int(input("Attempt {}, enter a number: ".format(i)))
+			if num > randomNumber:
+				print("\nThe number is less\n")
+				puntos=puntos-10
+				print("Points: ",puntos)
+				i=i+1
+			elif num<randomNumber:
+				print("\nThe number is greater\n")
+				puntos=puntos-10
+				print("Points: ",puntos,"\n")
+				i=i+1
+			else:
+				print("\nYOU HAVE WON IN",i,"ATTEMPTS.\n")
+				print("Final points: ",puntos,"\n")	
 		else:
-			print("\nYOU HAVE WON IN",i,"ATTEMPTS.\n")
-			print("Final points: ",puntos,"\n")	
-	else:
-		print("\nYou lost, the number was: ",randomNumber,"\n")
-		num=randomNumber
+			print("\nYou lost, the number was: ",randomNumber,"\n")
+			num=randomNumber
 
+	again=input("Do you want to play again?(y/n): ")
