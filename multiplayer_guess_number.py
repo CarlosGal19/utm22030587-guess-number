6dc8e83cc93a1854b6e4406cfb6b5b0e3d4e68c8
JacobEsc2004 <utm22030618@utma.edu.mx>
2023-06-06 21:45:40 -0600

    Feat: Se añade funcionalidad para ingresar el numero oculto

---
+++ multiplayer_guess_number.py
@@ -0,0 +1,10 @@
+import getpass
+
+print("\n***GUESS THE NUMBER MULTIPLAYER***\n")
+print("Instructions. Player 1 has to enter a number, and player 2 has to guess the number before he/she ran out of points.\n")
+
+hidNumber = getpass.getpass("(Player 1) Write the number to be guessed: ")
+hidNumber = int(hidNumber)
+
+
+            
