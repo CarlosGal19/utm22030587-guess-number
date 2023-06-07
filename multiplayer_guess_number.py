784d7f423a91f360438b90b5a677a58ba7cc2594
JacobEsc2004 <utm22030618@utma.edu.mx>
2023-06-06 21:52:12 -0600

    Feat: Se añade funcionalidad para encontrar el numero oculto y mostrar el ganador

--- multiplayer_guess_number.py
+++ multiplayer_guess_number.py
@@ -6,5 +6,29 @@ print("Instructions. Player 1 has to enter a number, and player 2 has to guess t
 hidNumber = getpass.getpass("(Player 1) Write the number to be guessed: ")
 hidNumber = int(hidNumber)
 
+p2Points = 100
+i=1
+guess=hidNumber+1
+
+while guess!=hidNumber: 
+        if p2Points>0:
+            guess = int(input("(Player 2) Attempt {}. Guess the number\n".format(i)))
+
+            if guess>hidNumber:
+                     print("\nThe number must be less\n")
+                     p2Points=p2Points-10
+                     print("Your current score is: ", p2Points)
+                     i=i+1
+            elif guess<hidNumber:
+                     print("\nThe number must be greater\n")
+                     p2Points=p2Points-10
+                     print("Your current score is: ", p2Points)
+                     i=i+1
+
+
+            else:
+                     print("\n(Player 2) You won with ", i, "attempts\n" "Your final score is: ", p2Points)
+        else:
+              print("Player 1 wins, the number was :", hidNumber)
 
             
